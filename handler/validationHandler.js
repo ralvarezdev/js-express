e509242c311c6139b9574311f68e351977a1999c
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-03-19 17:54:32 -0400

    fix: added null request body checker

--- handler/validationHandler.js
+++ handler/validationHandler.js
@@ -2,6 +2,10 @@ import {FailResponseError} from "../errors.js";
 
 // Handle the request validation
 export default function HandleValidation(req, res, validationFn = body => [body, true]) {
+    // Check if the request body is null
+    if (!req.body)
+        throw new FailResponseError(400, {body: "Body is null"});
+
     // Validate the request
     const [body, isValid] = validationFn(req.body);
 
